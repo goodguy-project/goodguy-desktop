3e68640dc2cfe7d68e8f212f1637b1086c267d65
ConanYu <c.zing@outlook.com>
2022-11-20 23:37:44 +0800

    feat: v0.1.1

--- app/server.ts
+++ app/server.ts
@@ -1,7 +1,7 @@
 import * as portfinder from "portfinder";
 import * as path from "path";
 import * as child_process from "child_process";
-import GetConfig from "./config";
+import {GetConfig, GetExternalResourcesRoot} from "./config";
 import axios from 'axios'; // bug with v1.1.3 https://github.com/axios/axios/issues/5011
 // const axios = require('axios'); // module not found after package
 
@@ -12,7 +12,7 @@ function sleep(ms: number) {
 export default async function Server(host: string) {
     const realHost = host === '0.0.0.0' ? '127.0.0.1' : host;
     const port = await portfinder.getPortPromise();
-    const exePath = path.join(process.cwd(), '.goodguy-desktop', 'backend', 'server.exe');
+    const exePath = path.join(GetExternalResourcesRoot(), 'backend', 'server.exe');
     const subprocess = child_process.spawn(exePath, [
         '--host', host, '--port', port.toString(),
     ], {cwd: path.dirname(exePath)});
